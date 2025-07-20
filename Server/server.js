5fbae76fa7bb09e1f0f70da803abe7c009e62e07
vanshsharma-tech <vanshsharma9431@gmail.com>
2025-07-20 12:40:38 +0530

    update origin

--- Server/server.js
+++ Server/server.js
@@ -8,10 +8,26 @@ import imageRouter from "./routes/imageRoute.js";
 const port = process.env.PORT || 4000;
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: "https://imagifyin.netlify.app", // or your frontend domain
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://imagifyin.netlify.app",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like curl or Postman)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
-}));
+};
+
+app.use(cors(corsOptions));
 connectDB()
 app.use('/api/user',userRouter)
 app.use('/api/image',imageRouter)
